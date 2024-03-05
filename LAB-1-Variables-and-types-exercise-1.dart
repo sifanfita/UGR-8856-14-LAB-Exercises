07f6ad5258642006e754376e58dd1d023dc5e92f
Sifan Fita Hika <sifanfita0@gmail.com>
2024-03-05 15:26:40 +0300

    variables and types exercise-1

---
+++ LAB-1-Variables-and-types-exercise-1.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Declare three variables and assign integer values
+  int a = 5;
+  int b = 10;
+  int c = 3;
+
+  // Calculate the sum
+  int sumOfVariables = a + b + c;
+
+  // Print the sum
+  print("The sum of a, b, and c is: $sumOfVariables");
+}
