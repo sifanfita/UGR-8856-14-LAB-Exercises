04da4b097c46a39006641308cc0ef82fe4d3fa76
Sifan Fita Hika <sifanfita0@gmail.com>
2024-03-05 15:35:01 +0300

    add code for exercise-2

---
+++ LAB-1-Variables-and-types-exercise-2.dart
@@ -0,0 +1,7 @@
+void main() {
+  // Create a variable str of type String and assign it a value
+  String str = "Hello, World!";
+  
+  // Print the length of the string
+  print("The length of the string is: ${str.length}");
+}
