34103377233bc482a25b10ddd3504c9b6679a4d9
Sifan Fita Hika <sifanfita0@gmail.com>
2024-03-05 15:45:40 +0300

    add code for exercise 1

---
+++ LAB-1-Control-Flow-Statements-exercise-1.dart
@@ -0,0 +1,30 @@
+void main() {
+  int day = 3; // Change this value to test different days
+
+  switch (day) {
+    case 1:
+      print("Monday");
+      break;
+    case 2:
+      print("Tuesday");
+      break;
+    case 3:
+      print("Wednesday");
+      break;
+    case 4:
+      print("Thursday");
+      break;
+    case 5:
+      print("Friday");
+      break;
+    case 6:
+      print("Saturday");
+      break;
+    case 7:
+      print("Sunday");
+      break;
+    default:
+      print(
+          "Invalid day"); // Print this message if the input is not from 1 to 7
+  }
+}
